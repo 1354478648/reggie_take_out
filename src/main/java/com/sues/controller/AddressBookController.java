c0ec85f998df9a2c4fdd24049319dd57f0d09833
Downey <1354478648@qq.com>
2023-07-11 15:19:17 +0800

    使用Sharding-JDBC实现程序的读写分离

--- src/main/java/com/sues/controller/AddressBookController.java
+++ src/main/java/com/sues/controller/AddressBookController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
